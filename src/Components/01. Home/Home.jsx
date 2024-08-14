d7528c953783af80e6e0d551da7ca71bb9c1afd6
Rakesh-TN <tnrakesh68@outlook.com>
2024-08-14 06:41:17 +0530

    add correction

--- src/Components/01. Home/Home.jsx
+++ src/Components/01. Home/Home.jsx
@@ -6,17 +6,19 @@ function Home() {
         <>
             <main className='bg-primary-subtle'>
                 <section className='d-flex flex-column justify-content-center align-items-center min-vh-100'>
+                    <div className='container madMax'>
                     <div className='row'>
-                        <div className='d-flex align-items-center'>
-                            <div className='col-lg-6'>
+                        <div className='d-flex col-lg-6 align-items-center'>
+                            <div className=''>
                                 <h1>Hi I'm <span className='text-primary'>Rakesh</span>👋</h1>
                                 <h3>Web Developer</h3>
                                 <p>Transforming ideas into elegant with a better User Interface </p>
                             </div>
-                            <div className='col-lg-6'>
-                                <img src={profilePic} alt='' className='w-75 rounded-circle'/>
-                            </div>
                         </div>
+                        <div className='col-lg-6'>
+                            <img src={profilePic} alt='' className='w-75 rounded-circle' />
+                        </div>
+                    </div>
                     </div>
                 </section>
             </main>
