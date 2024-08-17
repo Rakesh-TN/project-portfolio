0651be0d02a05fe909f6dcc48c56b4937b87cd14
Rakesh-TN <tnrakesh68@outlook.com>
2024-08-17 13:29:10 +0530

    all

--- src/Components/02. Project/Project.jsx
+++ src/Components/02. Project/Project.jsx
@@ -5,6 +5,7 @@ import ecomSite from './Assets/Ecom-Site.svg'
 import weatherSite from './Assets/Weather-Site.svg'
 import BMISite from './Assets/BMI_calculator.svg'
 import portfolioSite from './Assets/portfolio_website.svg'
+import wilsonSite from './Assets/eating_together.svg'
 
 
 function Project() {
@@ -13,8 +14,8 @@ function Project() {
       <main className='bg-primary-subtle'>
         <section className='d-flex flex-column justify-content-center align-items-center min-vh-100'>
         <h1>Project</h1>
-          <div className='row'>
-          <Card style={{ width: '18rem' }} className='col-lg-4 p-1 m-5 row justify-content-evenly'>
+          <div className='row mx-5'>
+          <Card style={{ width: '18rem' }} className='col-lg-4 m-5 row justify-content-evenly'>
             <Card.Img variant="top" src={ecomSite} />
             <Card.Body>
               <Card.Title>E-Commerce Site</Card.Title>
@@ -24,7 +25,7 @@ function Project() {
               <Button variant="primary" href='https://subtle-toffee-c03964.netlify.app/' target='_blank'>Click Here</Button>
             </Card.Body>
           </Card>
-          <Card style={{ width: '18rem' }} className='col-lg-4 p-1 m-5'>
+          <Card style={{ width: '18rem' }} className='col-lg-4 p-1 m-5 justify-content-evenly'>
             <Card.Img variant="top" src={weatherSite}/>
             <Card.Body>
               <Card.Title>Weather Site</Card.Title>
@@ -34,7 +35,7 @@ function Project() {
               <Button variant="primary" href='https://weather-finder-369.netlify.app/' target='_blank'>Click Here</Button>
             </Card.Body>
           </Card>
-          <Card style={{ width: '18rem' }} className='col-lg-4 p-1 m-5'>
+          <Card style={{ width: '18rem' }} className='col-lg-4 p-1 m-5 justify-content-evenly'>
             <Card.Img variant="top" src={BMISite}/>
             <Card.Body>
               <Card.Title>BMI Calculator</Card.Title>
@@ -44,7 +45,7 @@ function Project() {
               <Button variant="primary" href='https://effervescent-squirrel-4ab9f2.netlify.app/' target='_blank'>Click Here</Button>
             </Card.Body>
           </Card>
-          <Card style={{ width: '18rem' }} className='col-lg-4 p-1 m-5'>
+          <Card style={{ width: '18rem' }} className='col-lg-4 p-1 m-5 justify-content-evenly'>
             <Card.Img variant="top" src={portfolioSite}/>
             <Card.Body>
               <Card.Title>Old Portfolio</Card.Title>
@@ -52,6 +53,16 @@ function Project() {
               This Site is build with HTML, CSS, ReactJS and EmailJS
               </Card.Text>
               <Button variant="primary" href='https://old-portfolio-six-jade.vercel.app/' target='_blank'>Click Here</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }} className='col-lg-4 p-1 m-5 justify-content-evenly'>
+            <Card.Img variant="top" src={wilsonSite}/>
+            <Card.Body>
+              <Card.Title>Wilson Bread</Card.Title>
+              <Card.Text>
+              This Site is build with HTML, CSS
+              </Card.Text>
+              <Button variant="primary" href='https://gorgeous-cucurucho-e25b1b.netlify.app/' target='_blank'>Click Here</Button>
             </Card.Body>
           </Card>
           </div>
